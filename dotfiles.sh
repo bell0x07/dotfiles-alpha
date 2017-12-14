d9b9917e3fbbd3880a61d004454c4aaf0c5a6c87
Jakub Kowalik <bell0x07@users.noreply.github.com>
2017-12-14 15:24:34 +0100

    Update dotfiles.sh

--- dotfiles.sh
+++ dotfiles.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-ln -s -v ~/.dotfiles/bell/.zshrc ~
-ln -s -v ~/.dotfiles/bell/.gitconfig ~
-ln -s -v ~/.dotfiles/bell/.nanorc ~
-ln -s -v ~/.dotfiles/bell/ssh/config ~/.ssh/config
-ln -s -v ~/.dotfiles/bell/ssh/authorized_keys ~/.ssh/authorized_keys
-ln -s -v ~/.dotfiles/bell/.vimrc ~
+ln -s -v ~/dotfiles-alpha/bell/.zshrc ~
+ln -s -v ~/dotfiles-alpha/bell/.gitconfig ~
+ln -s -v ~/dotfiles-alpha/bell/.nanorc ~
+ln -s -v ~/dotfiles-alpha/bell/ssh/config ~/.ssh/config
+ln -s -v ~/dotfiles-alpha/bell/ssh/authorized_keys ~/.ssh/authorized_keys
+ln -s -v ~/dotfiles-alpha/bell/.vimrc ~
 
